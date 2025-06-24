fe565087f0e39beb26be80b75a58be9750c55cc7
Dharvipatel10 <dharvipatel1610@gmail.com>
2025-06-24 12:15:03 +0200

    done with mobile menu

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -3,11 +3,12 @@
     'icon' => null,
     'bgClass' => 'bg-yellow-500',
     'hoverClass' => 'hover:bg-yellow-600',
-    'textClass' => 'text-black'
+    'textClass' => 'text-black',
+    'block' => false,
 ])
 
 <a href="{{ $url }}"
-   class="inline-flex items-center {{ $bgClass }} {{ $hoverClass }} {{ $textClass }} px-4 py-2 rounded transition duration-300">
+   class="{{ $bgClass }} {{ $hoverClass }} {{ $textClass }} px-4 py-2 rounded transition duration-300 {{$block ? 'block' : ''}}">
    
    @if ($icon)
        <i class="fa fa-{{ $icon }} mr-2"></i>
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,7 +1,7 @@
-@props(['url' => '/', 'active' => false, 'icon' => null, 'mobile' => false])
+@props(['url' => '/', 'active' => false, 'icon' => null, 'mobile' => null])
 
 @if($mobile)
-<a href="{{$url)}}" class="block px-4 py-2 hover:bg-blue-700 {{$active ? 'text-yellow-500 font-bold' : ''}}">
+<a href="{{$url}}" class="block px-4 py-2 hover:bg-blue-700 {{$active ? 'text-yellow-500 font-bold' : ''}}">
     @if($icon)
         <i class="fa fa-{{$icon}}" mr-1"></i>
     @endif

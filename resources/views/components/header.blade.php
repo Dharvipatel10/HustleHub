45733ea49c04541b75176e1f4617965255b642df
Dharvipatel10 <dharvipatel1610@gmail.com>
2025-06-24 11:55:36 +0200

    Change mobile Menu nav-link

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -28,18 +28,12 @@
             <!-- Mobile Menu -->
     <nav
         id="mobile-menu"
-        class="hidden md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2">
+        class="md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2">
         <a href="{{url('/jobs')}}" class="block px-4 py-2 hover:bg-blue-700">All Jobs</a>
-        <a
-            href="{{url('/jobs/saved')}}"
-            class="block px-4 py-2 hover:bg-blue-700">Saved Jobs</a>
+        <a href="{{url('/jobs/saved')}}" class="block px-4 py-2 hover:bg-blue-700">Saved Jobs</a>
         <a href="{{url('/login')}}" class="block px-4 py-2 hover:bg-blue-700">Login</a>
-        <a
-            href="{{url('/register')}}"
-            class="block px-4 py-2 hover:bg-blue-700">Register</a>
-        <a
-            href="{{url('/dsashboard')}}"
-            class="block text-white hover:underline py-2">
+        <a href="{{url('/register')}}" class="block px-4 py-2 hover:bg-blue-700">Register</a>
+        <a href="{{url('/dsashboard')}}" class="block text-white hover:underline py-2">
             <i class="fa fa-gauge mr-1"></i> Dashboard
         </a>
         <x-button-link url="/jobs/create" icon="edit">CreateJob</x-button-link>

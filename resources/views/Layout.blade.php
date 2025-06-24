95669749ef72872dd062c7d5154295a88a9c4c90
Dharvipatel10 <dharvipatel1610@gmail.com>
2025-06-24 16:14:33 +0200

    Hero Component is set up

--- resources/views/Layout.blade.php
+++ resources/views/Layout.blade.php
@@ -9,7 +9,11 @@
 </head>
 
 <body class="bg-gray-100">
-    <x-header></x-header>
+    <x-header />
+    @if(request()->is('/'))
+    <x-hero/>
+    <x-top-banner />
+    @endif
     <main class="container mx-auto p-4 mt-4">
         {{$slot}}
     </main>

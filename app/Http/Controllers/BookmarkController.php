97f72762a54f70029bf6593212dd6c3aad209189
Dharvipatel10 <dharvipatel1610@gmail.com>
2025-07-02 19:33:05 +0200

    Implemented Remove Bookmark Listing button

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -26,12 +26,28 @@ class BookmarkController extends Controller
 
         //Check if the job is already bookmarked
         if($user->bookmarkedJobs()->where('job_id', $job->id)->exists()) {
-            return back()->where('status', 'Job is already bookmarked!');
+            return back()->with('error', 'Job is already bookmarked!');
         }
 
         //Create new bookmark
         $user->bookmarkedJobs()->attach($job->id);
 
         return back()->with('success', 'Job bookmarked sucessfully!');
+    }
+
+    //@desc  Remove bookmarked job
+    //@route  DELETE/bookmarks/{jobs}
+    public function destroy(Job $job): RedirectResponse{
+        $user = Auth::user();
+
+        //Check if the job is not bookmarked
+        if(!$user->bookmarkedJobs()->where('job_id', $job->id)->exists()) {
+            return back()->with('error', 'Job is not bookmarked!');
+        }
+
+        // Remove bookmark
+        $user->bookmarkedJobs()->detach($job->id);
+
+        return back()->with('success', 'Bookmark remove sucessfully!');
     }
 }

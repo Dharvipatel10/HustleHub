c6c3e657658b48ee4ee603006d181a2d29e8e136
Dharvipatel10 <dharvipatel1610@gmail.com>
2025-07-17 11:26:43 +0200

    railway.json

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,19 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        outDir: 'public/build',
+        emptyOutDir: true,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
+        },
+    },
 });
